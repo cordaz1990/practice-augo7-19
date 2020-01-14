c96665434208fed9bccf55c66fd2d7ee960adc4f
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:31:38 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,14 @@
-print('hello')
-print('world')
-print()
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+          d[c] += 1
+    return d
+  
+>>> h = histogram('brontosaurus')
+>>> h
+{'a':1, 'b':1, 'o': 2, 'n':1, 's':2, 'r':2, 'u':2, 't':1}
+
+          
